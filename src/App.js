c1ad3eff41d5397a98d104c2f2118bcc4d6cee87
betul <bsenger@aof.anadolu.edu.tr>
2023-03-26 20:38:13 +0300

    feat: form

--- src/App.js
+++ src/App.js
@@ -1,17 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
-
+import { useState } from "react"
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-    </header>
-    </div>
-  );
+  const [inputs, inputsUpdate] = useState({})
+
+  function formUpdate(event) {
+    const name = event.target.name;
+    const value = event.target.value;
+    inputsUpdate(values => ({ ...values, [name]: value }))
+  }
+
+  function formSubmit(event) {
+    event.preventDefault()
+
+    if( inputs.age < 18 ) {
+        alert("age not suitable ")
+    } else {
+        event.target.submit()
+    }
 }
 
-export default App;
+
+  return (
+    <form className="col-md-4" onSubmit={formSubmit}>
+      <label className="mt-3 mx-3">Enter your name:
+        <input
+          className="form-control"
+          type="text"
+          name="name"
+          value={inputs.name || ""}
+          onChange={formUpdate}
+        />
+      </label>
+
+      <label className="mt-3 mx-3"> Enter your age:
+        <input
+          className="form-control"
+          type="number"
+          name="age"
+          value={inputs.age || ""}
+          onChange={formUpdate}
+        />
+      </label>
+
+      <div className="col-md-4">
+        <input className="btn btn-success" type="submit" />
+      </div> 
+   </form>
+  )
+}
+
+export default App
