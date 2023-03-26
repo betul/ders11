a1139458c6491bb8250fcf54a0cb0308b56e6064
betul <bsenger@aof.anadolu.edu.tr>
2023-03-26 20:08:57 +0300

    feat: short added

--- src/ContactForm.jsx
+++ src/ContactForm.jsx
@@ -76,3 +76,87 @@ function ContactForm() {
 
 
 export default ContactForm
+
+
+/* import { useState } from "react"
+
+function ContactForm() {
+    const [name, setName] = useState("")
+    const [surname, setSurname] = useState("")
+    const [birth, setBirth] = useState("")
+    const [phone, setPhone] = useState("")
+
+    const handleNameChange = (event) => setName(event.target.value)
+    const handleSurnameChange = (event) => setSurname(event.target.value)
+    const handleBirthChange = (event) => setBirth(event.target.value)
+    const handlePhoneChange = (event) => setPhone(event.target.value)
+
+    const nameError = name.includes(".")
+    const surnameError = surname.includes(".")
+    const birthError = birth.includes(".")
+    const phoneError = phone.includes(".")
+
+    const handleSubmit = (event) => {
+        event.preventDefault()
+
+        if (birth.length !== 4) {
+            alert("Year of birth must be 4 characters.")
+        } else {
+            event.target.submit()
+        }
+    }
+
+    return (
+        <form className="row" onSubmit={handleSubmit}>
+            <div className="col-md-6 my-5">
+                <input
+                    value={name}
+                    onChange={handleNameChange}
+                    type="text"
+                    className="form-control"
+                    placeholder="Name"
+                />
+                <label>{nameError ? "Name cannot contain '.'" : ""}</label>
+
+                <input
+                    value={surname}
+                    onChange={handleSurnameChange}
+                    type="text"
+                    className="form-control mt-2"
+                    placeholder="Surname"
+                />
+                <label>{surnameError ? "Surname cannot contain '.'" : ""}</label>
+
+                <input
+                    value={birth}
+                    onChange={handleBirthChange}
+                    type="text"
+                    className="form-control mt-2"
+                    placeholder="Year of Birth"
+                />
+                <label>{birthError ? "Birth cannot contain '.'" : ""}</label>
+
+                <input
+                    value={phone}
+                    onChange={handlePhoneChange}
+                    type="text"
+                    className="form-control mt-2"
+                    placeholder="Phone"
+                />
+                <label>{phoneError ? "Phone cannot contain '.'" : ""}</label>
+
+                <button className="btn btn-success mt-4" type="submit">Submit</button>
+            </div>
+
+            <div className="col-md-6 my-5">
+                <p>Name: {name}</p>
+                <p>Surname: {surname}</p>
+                <p>Birth Date: {birth}</p>
+                <p>Phone: {phone}</p>
+            </div>
+        </form>
+    )
+}
+
+export default ContactForm
+*/
